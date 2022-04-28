9ac6e7274b2145160e3e3add2c90a8b2c6241850
guoyubo <guoyubo@codemao.cn>
2022-04-28 14:45:10 +0800

    chore: add commitlint to force commit style

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const obj = {
+  long: 'dffdskaldfjdslka;jdfdsakjfkl;ewjqrkewl;qruewiuqriowuriewuriouewioqurioeuwqroiuewnrewqrewqrewqrewqvnds,mnf,n',
+  name: 'vortesnail',
+  age: 25,
+  skill: (a) => {
+    console.log('sleep');
+  },
+};
